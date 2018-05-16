bd9bac7171417221ad730c08a35c9e7792f00591
Shawn Salat <shawn.salat@gmail.com>
2018-05-16 14:14:11 -0700

    set up server to reply with path

--- bin/p01_basic_server.rb
+++ bin/p01_basic_server.rb
@@ -4,7 +4,8 @@ app = Proc.new do |env|
   req = Rack::Request.new(env)
   res = Rack::Response.new
   res['Content-Type'] = 'text/html'
-  res.write("Hello world!")
+  # puts req.attributes
+  res.write(req.path)
   res.finish
 end
 
